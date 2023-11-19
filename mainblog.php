1f942e547fdbeaeb467ee6ac854cdf7decce9fd9
Farish Jamal <farishjamal98@gmail.com>
2023-11-20 02:00:55 +0530

    Routing To Particular Blog

--- mainblog.php
+++ mainblog.php
@@ -18,12 +18,33 @@
               <p><b>'.$row['author'].'</b></p>
               <p><b>'.$row['date'].'</b></p>
             </div>
-            <a href="./fullblogpage.php" class="btn btn-primary" style="width: 100%">Read Full Blog</a>
+            <a href="/blogs/index.php?show='.$row['sno'].'" class="btn btn-primary" style="width: 100%">Read Full Blog</a>
           </div>
         </div>';
         }
       }
     }
+  }else if(isset($_GET['show'])){
+      $show_id = $_GET['show'];
+      $sql = "SELECT * FROM `blogdetail` WHERE `sno` = '$show_id'";
+      $result = mysqli_query($connection, $sql);
+
+      if($result){
+        foreach ($result as $row) {
+            echo '<div class="container mt-4">
+            <h2 class="text-center my-5">'.$row['title'].'</h2>
+            <div class="mx-5 d-flex justify-content-between align-items-center my-3">
+              <p style="font-weight: 700">'.$row['author'].'</p>
+              <p style="font-weight: 700">'.$row['date'].'</p>
+            </div>
+            <p class="text-center mb-5">'.$row['description'].'</p>
+            <div class="d-flex justify-content-center">
+                <button class="btn btn-primary mx-2 edit">Edit This Blog</button>
+                <button class="btn btn-danger edit">Delete This Blog</button>
+            </div>
+            </div>';
+        }
+      }
   }else{
     $sql = "SELECT * FROM `blogdetail`";
     $result = mysqli_query($connection,$sql);
@@ -38,10 +59,81 @@
             <p><b>'.$row['author'].'</b></p>
             <p><b>'.$row['date'].'</b></p>
           </div>
-          <a href="./fullblogpage.php" class="btn btn-primary" style="width: 100%">Read Full Blog</a>
+          <a href="/blogs/index.php?show='.$row['sno'].'" class="btn btn-primary" style="width: 100%">Read Full Blog</a>
         </div>
       </div>';
       }
     }
   }
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+  <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="modal-close" data-dismiss="modal" aria-label="Close" id="closeModal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form method="post" action="/blogs/addnewblog.php">
+              <div class="mb-3">
+                <label for="title" class="form-label">Title For Blog</label>
+                <input type="text" class="form-control" id="titleEdit" name="titleEdit">
+              </div>
+              <div class="mb-3">
+                <label for="author" class="form-label">Author Name</label>
+                <input type="text" class="form-control" id="authorEdit" name="authorEdit">
+              </div>
+              <div class="mb-3">
+                <label for="description" class="form-label">Write Description For Blog</label>
+                <textarea class="form-control" name="descriptionEdit" id="descriptionEdit" style="height: 150px"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">Update Your Blog</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script>
+        edits = document.getElementsByClassName("edit");
+        // console.log(edits);
+        close = document.getElementById("closeModal");
+        close.addEventListener('click', ()=>{
+          $('#editModal').modal('hide');
+        })
+        Array.from(edits).forEach((element) => {
+          element.addEventListener('click', (e)=>{
+            content = e.target.parentNode.parentNode;
+            title = content.getElementsByTagName("h2")[0].innerText;
+            description = content.getElementsByTagName("p")[2].innerText;
+            author = content.getElementsByTagName("p")[0].innerText;
+            date = content.getElementsByTagName("p")[1].innerText;
+            // console.log(title, description, author, date);
+            titleEdit.value = title;
+            descriptionEdit.value = description;
+            authorEdit.value = author;
+            $('#editModal').modal('toggle');
+          })
+        });
+    </script>
+  </body>
+</html>
